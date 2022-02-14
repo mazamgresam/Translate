3f2ec3f37e28e6ad4dcb46b5a431655de0f9e36e
azadra527@gmail.com <azadra527@gmail.com>
2022-02-14 19:17:14 +0700

    add home

---
+++ translate-app/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header class="bg-gradient-to-b from-gray-900 to-gray-600 ">
+      <img class="w-1/6" src="../assets/logo.png" alt="logo">
+    </header>
+    <Translate />
+  </div>
+</template>
+
+<script>
+import Translate from "../components/Translate.vue";
+
+export default {
+    components: { Translate }
+}
+</script>
